3365af1d086d35063d339394276c17fc0c436104
Egor Vasilyev <e.s.vasilyev@mail.ru>
2024-07-28 20:06:56 +0300

    improve tests

--- chart/auto-tests/test_rate-limits-exporter.py
+++ chart/auto-tests/test_rate-limits-exporter.py
@@ -9,8 +9,14 @@ url = os.getenv('APP_URL', 'http://0.0.0.0:8080')
 def test_status():
     r = requests.get(url)
     assert r.status_code == 200
+    assert r.encoding == 'utf-8'
 
 
-def test_root():
-    r = requests.get(url)
-    assert re.search(r'metrics', r.text)
+def test_metrics():
+    r = requests.get(f'{url}/metrics')
+    assert re.search(r'dockerhub_ratelimit_scrape_error\{.*\} 0|1', r.text)
+
+
+def test_healthz():
+    r = requests.get(f'{url}/healthz')
+    assert r.text == 'Ok'
